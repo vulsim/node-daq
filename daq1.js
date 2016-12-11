413e7e4487de92cb508aa0a0eaf62ac84af6c855
Igor Mikheiko <vulsim@mail.ru>
2016-12-11 13:56:32 +0300

    Added support TC05

---
+++ daq1.js
@@ -0,0 +1,238 @@
+
+var schedule = require("node-schedule");
+var Influx = require("influx");
+var SerialPort = require('serialport');
+var util = require("util");
+var TC05 = require("./helpers/tc05").TC05;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var influxHost = "192.168.1.111";
+var influxDatabase = "garant";
+var influxDatabaseUser = "daq";
+var influxDatabasePassword = "influx";
+var influxNode = "vrb86_1"
+var serialDevice1 = "/dev/ttyUSB0";
+
+var logMessage1 = "->\tRead data from device\t\t\t\t%s";
+var logMessage2 = "->\tStarting of scheduled devices polling\t\t%s";
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var influx = new Influx.InfluxDB({
+	host: influxHost,
+	database: influxDatabase,
+	username: influxDatabaseUser,
+  	password: influxDatabasePassword,
+	schema: [
+		{
+			measurement: util.format("%s.heatmeter.info", influxNode),
+			tags: [],
+			fields: {
+				device_type: Influx.FieldType.STRING,
+				device_serial: Influx.FieldType.INTEGER,
+				device_date: Influx.FieldType.STRING,
+				fw_version: Influx.FieldType.INTEGER,
+				operating_hours: Influx.FieldType.INTEGER,
+				error_0: Influx.FieldType.INTEGER,
+				error_1: Influx.FieldType.INTEGER,
+				error_2: Influx.FieldType.INTEGER,
+				error_3: Influx.FieldType.INTEGER,
+				error_4: Influx.FieldType.INTEGER,
+				error_5: Influx.FieldType.INTEGER,
+				error_6: Influx.FieldType.INTEGER,
+				error_7: Influx.FieldType.INTEGER
+			}
+		},
+		{
+			measurement: util.format("%s.heatmeter.g", influxNode),
+			tags: [],
+			fields: {
+				device_serial: Influx.FieldType.INTEGER,
+				value: Influx.FieldType.FLOAT
+			}
+		},
+		{
+			measurement: util.format("%s.heatmeter.q", influxNode),
+			tags: [],
+			fields: {
+				device_serial: Influx.FieldType.INTEGER,
+				value: Influx.FieldType.FLOAT
+			}
+		},
+		{
+			measurement: util.format("%s.heatmeter.v", influxNode),
+			tags: [],
+			fields: {
+				device_serial: Influx.FieldType.INTEGER,
+				value: Influx.FieldType.FLOAT
+			}
+		},
+		{
+			measurement: util.format("%s.heatmeter.t1", influxNode),
+			tags: [],
+			fields: {
+				device_serial: Influx.FieldType.INTEGER,
+				value: Influx.FieldType.FLOAT
+			}
+		},
+		{
+			measurement: util.format("%s.heatmeter.t2", influxNode),
+			tags: [],
+			fields: {
+				device_serial: Influx.FieldType.INTEGER,
+				value: Influx.FieldType.FLOAT
+			}
+		}
+	]
+});
+
+var rawReadBuffer1 = null;
+
+var tc05 = new TC05("TC05", {
+	journal: {
+		information: function(message) {
+			console.log("INFO\t" + message);
+		},
+
+		warning: function(message) {
+			console.log("WARN\t" + message);
+		},
+
+		error: function(message) {
+			console.log("ERROR\t" + message);
+		}
+	}
+});
+
+var port1 = new SerialPort(serialDevice1, {
+  baudRate: 9600,
+  dataBits: 8,
+  parity: "none",
+  stopBits: 1,
+  autoOpen: false
+});
+
+port1.on("data", function(data) {
+    if (rawReadBuffer1 != null) {
+        rawReadBuffer1 = Buffer.concat([rawReadBuffer1, data]);
+    } else {
+        rawReadBuffer1 = data;
+    }
+});
+
+port1.on("open", function() {
+
+	var readHandler = function(timeout, rawReadCb) {
+		try {
+			var rawReadTimerId = setInterval(function() {
+				try {
+					clearInterval(rawReadTimerId);
+
+					if (rawReadBuffer1 != null) {
+						rawReadCb(null, rawReadBuffer1);
+						rawReadBuffer1 = null;
+					} else {
+						rawReadCb(null, null);
+					}			
+				} catch (e) {
+					rawReadCb(e, null);
+				}
+			}, timeout);
+		} catch (e) {
+			rawReadCb(e, null);
+		}						
+	};
+
+	var writeHandler = function (data, rawWriteCb) {
+		try {													
+			port1.write(data, function(err, length) {
+				rawWriteCb(err);
+			});
+		} catch (e) {
+			rawWriteCb(e);
+		}						
+	};
+
+	tc05.getDeviceType(readHandler, writeHandler, function (err, data) {
+		port1.close();
+
+		if (err) {
+		    console.log(util.format(logMessage1, "FAILED"));		    
+		} else {
+			console.log(util.format(logMessage1, "OK"));
+			//console.log(util.format("\t\\- Device serial: %d, fw: %d, errors: %d", data.device_serial, data.fw_version, data.errors.length));
+
+			console.log(data);
+			
+			/*influx.writePoints([
+				{
+					measurement: util.format("%s.heatmeter.info", influxNode),
+				    fields: {
+				    	device_type: "tem05m1",
+				    	device_serial: data.device_serial,
+				    	device_date: data.date.toString(),					
+						fw_version: data.fw_version,
+						operating_hours: data.operating_hours,
+						error_0: (data.errors.indexOf("t_sensor_failure") > -1) ? 1 : 0,
+						error_1: (data.errors.indexOf("flow_or_pressure_sensor_failure") > -1) ? 1 : 0,
+						error_2: (data.errors.indexOf("g1_under_min") > -1) ? 1 : 0,
+						error_3: (data.errors.indexOf("g2_under_min") > -1) ? 1 : 0,
+						error_4: (data.errors.indexOf("g1_over_max") > -1) ? 1 : 0,
+						error_5: (data.errors.indexOf("g2_over_max") > -1) ? 1 : 0,
+						error_6: (data.errors.indexOf("dt_under_min") > -1) ? 1 : 0,
+						error_7: (data.errors.indexOf("power_failure") > -1 ? 1 : 0)
+				    }
+				},
+				{
+					measurement: util.format("%s.heatmeter.g", influxNode),
+				    fields: { 
+				    	device_serial: data.device_serial,
+						value: data.g1
+				    }
+				},
+				{
+					measurement: util.format("%s.heatmeter.q", influxNode),
+				    fields: { 
+				    	device_serial: data.device_serial,
+						value: data.q1
+				    }
+				},
+				{
+					measurement: util.format("%s.heatmeter.v", influxNode),
+				    fields: { 
+				    	device_serial: data.device_serial,
+						value: data.v1
+				    }
+				},
+				{
+					measurement: util.format("%s.heatmeter.t1", influxNode),
+				    fields: { 
+				    	device_serial: data.device_serial,
+						value: data.t1
+				    }
+				},
+				{
+					measurement: util.format("%s.heatmeter.t2", influxNode),
+				    fields: { 
+				    	device_serial: data.device_serial,
+						value: data.t2
+				    }
+				}
+			]);*/
+		}
+	});
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var daq_job = schedule.scheduleJob("*/10 * * * *", function() {
+	try {
+		port1.open();
+		console.log(util.format(logMessage2, "OK"));
+	} catch (err) {
+		console.log(util.format(logMessage2, "FAILED"));
+	}	
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ helpers/tc05.js
@@ -0,0 +1,118 @@
+var Class = require("../core/class");
+var Object = require("../core/object");
+
+var CCore = require("../helpers/ccore").CCore;
+var CJournal = require("../helpers/cjournal").CJournal;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var util = require("util");
+var crc = require("../helpers/crc");
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var TC05 = Class.Inherit("TC05", Object, function (name, context) {
+	
+	Class.Construct(this, name);
+
+	this.core = new CCore("CCoreHelper", context);
+	this.journal = new CJournal("CJournalHelper", context);
+
+	return this;
+});
+
+TC05.prototype.rawRequestPacket = function (address, func, param, dummy1, dummy2, dummy3, dummy4) {
+
+	var buffer = new Buffer((func > 192) ? 9 : 8);
+	var crcSum = 0xFFFF;
+
+	buffer[0] = parseInt(address);
+	buffer[1] = parseInt(func);
+	buffer[2] = parseInt(param);
+	
+	buffer[3] = parseInt(dummy1);
+	buffer[4] = parseInt(dummy2);
+	buffer[5] = parseInt(dummy3);
+
+	if (func > 192) {
+		buffer[6] = parseInt(dummy4);		
+	}
+
+	for (var i = 0; i < buffer.length - 2; i++) {
+		crcSum = crc.crc16_ARC_Add(crcSum, buffer[i]);
+	}
+
+	buffer[buffer.length - 2] = (crcSum >> 8) & 0xFF;
+	buffer[buffer.length - 1] = crcSum & 0xFF;
+
+	return buffer;
+};
+
+TC05.prototype.rawResponsePacket = function (buffer) {
+
+	if (buffer && buffer.length > 5) {
+		var crcSum = 0xFFFF;
+
+		for (var i = 0; i < buffer.length - 2; i++) {
+			crcSum = crc.crc16_ARC_Add(crcSum, buffer[i]);
+		}
+
+		if ((parseInt(buffer[buffer.length - 2]) << 8) + (parseInt(buffer[buffer.length - 1])) == crcSum) {
+			var ret = {
+				"address" : buffer[0],
+				"func" : buffer[1],
+				"param" : buffer[2],
+				"result" : buffer[3]
+			};
+
+			if (buffer.length > 6) {
+				var data = new Buffer(buffer.length - 6);
+
+				for (var i = 0; i < buffer.length - 6; i++) {
+					data[i] = buffer[i + 4];
+				}
+
+				ret.data = data;
+			}
+
+			return ret;
+		}		
+	}
+	
+	return null;
+};
+
+TC05.prototype.getDeviceType = function (rawReadFunc, rawWriteFunc, devId, cb) {
+
+	var that = this;
+
+	try {
+		rawWriteFunc(that.rawRequestPacket(devId, 3, 17, 0, 0 ,0, 0), function (err) {
+			try {
+				rawReadFunc(250, function (err, rawData) {
+					try {
+						var data = that.rawResponsePacket(rawData);
+						if (data && data.data && data.func == 3 && data.param == 17 && data.result == 0) {
+							cb(null, data.data.toString());
+						} else {
+							cb(new Error("An error occurred when reading from device"));
+						}
+					} catch (e) {
+						that.journal.error(e.stack.toString());
+						cb(e);
+					}
+				});
+			} catch (e) {
+				that.journal.error(e.stack.toString());
+				cb(e);
+			}
+		});
+	} catch (e) {
+		that.journal.error(e.stack.toString());
+		cb(e);
+	}
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+module.exports.TC05 = TC05;
